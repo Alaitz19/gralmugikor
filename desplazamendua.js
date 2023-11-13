5d0e8e3b48f2a10fbd4dba99b1fef4cd60d85126
Alaitz <87779550+Alaitz19@users.noreply.github.com>
2023-11-13 14:35:40 +0100

    Update desplazamendua.js

--- desplazamendua.js
+++ desplazamendua.js
@@ -1,1 +1,37 @@
+let contenedor = document.getElementById('contenedor');
+let paginaActual = 1;
+
+document.addEventListener('touchstart', handleTouchStart, false);
+document.addEventListener('touchmove', handleTouchMove, false);
+
+let x1 = null;
+
+function handleTouchStart(event) {
+    x1 = event.touches[0].clientX;
+}
+
+function handleTouchMove(event) {
+    if (!x1) {
+        return;
+    }
+
+    let x2 = event.touches[0].clientX;
+    let deltaX = x2 - x1;
+
+    if (Math.abs(deltaX) > 50) { // Se considera un desplazamiento significativo
+        if (deltaX > 0 && paginaActual > 1) {
+            paginaActual--;
+        } else if (deltaX < 0 && paginaActual < 2) {
+            paginaActual++;
+        }
+
+        updateContenedor();
+        x1 = null;
+    }
+}
+
+function updateContenedor() {
+    let nuevaTransformacion = 'translateX(' + (paginaActual - 1) * -50 + '%)';
+    contenedor.style.transform = nuevaTransformacion;
+}
 
